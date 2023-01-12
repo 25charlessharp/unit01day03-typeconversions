d201fdae833ae8b8c6f063a7780466625c142194
Charles Sharp <25charlessharp@usd266.com>
2023-01-12 11:26:00 -0600

    Actually Finished

--- TrigIdentity.java
+++ TrigIdentity.java
@@ -1,11 +1,6 @@
-class Main{
+class Main {
     public static void main(String [] args) {
-      double a = Double.parseDouble(args[0]);
-      double b = Double.parseDouble(args[1]);
-      double c = Double.parseDouble(args[2]);
-      double d = (-b + Math.sqrt(b*b + -4*a*c))/2*a;
-      double e = (-b - Math.sqrt(b*b + -4*a*c))/2*a;
-  
-      System.out.println("The first answer is " + d );
-      System.out.println("The second answer is "+ e);
-    }}
+      double theta = Double.parseDouble(args[0]);
+      System.out.println(Math.pow(java.lang.Math.sin(theta),2) +   Math.pow(java.lang.Math.cos(theta),2));
+    }
+  }
