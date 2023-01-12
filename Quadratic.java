2e7aecd18e69873e446135124d1b19ae26e5e12e
Charles Sharp <25charlessharp@usd266.com>
2023-01-12 11:18:48 -0600

    Finished

--- Quadratic.java
+++ Quadratic.java
@@ -1,5 +1,6 @@
-public class Quadratic {
-    public static void main(String[] args) {
-        
+class Main {
+    public static void main(String [] args) {
+      double theta = Double.parseDouble(args[0]);
+      System.out.println(Math.pow(java.lang.Math.sin(theta),2) +   Math.pow(java.lang.Math.cos(theta),2));
     }
-}
+  }
